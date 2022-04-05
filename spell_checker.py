47b0831814baaad25a864f9915ee5cb9968613f8
panharathsok3 <100652520+panharathsok3@users.noreply.github.com>
2022-04-05 18:54:28 -0400

    add test

--- spell_checker.py
+++ spell_checker.py
@@ -1,6 +1,13 @@
-from english_words import english_words_set
+from english_words import english_words_set as ews
+import test
 
 
 class Spellchecker:
     def __init__(self, word):
         self.word = word
+
+    def english_word(self):
+        if self.word in ews:
+            print('nice')
+
+
